4f225b3614cfc448a0080ad38b8fbb2b9bac10b3
Aleksey Yablokov <ya_al@bk.ru>
2017-11-30 21:24:10 +0300

    add SpringSecurityInitializer

---
+++ src/main/java/booking/web/security/SpringSecurityInitializer.java
@@ -0,0 +1,10 @@
+package booking.web.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * @author Aleksey Yablokov
+ */
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+    //do nothing
+}
