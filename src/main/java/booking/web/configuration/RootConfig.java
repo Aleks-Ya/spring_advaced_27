32c86357d3341bec898394579323bf241c00be19
Aleksey Yablokov <ya_al@bk.ru>
2017-12-02 08:16:03 +0300

    enable AOP by annotation

--- src/main/java/booking/web/configuration/RootConfig.java
+++ src/main/java/booking/web/configuration/RootConfig.java
@@ -2,11 +2,13 @@ package booking.web.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * @author Aleksey Yablokov
  */
 @Configuration
 @ComponentScan("booking")
+@EnableAspectJAutoProxy
 public class RootConfig {
 }
