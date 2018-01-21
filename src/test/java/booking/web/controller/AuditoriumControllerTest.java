68865a7719f9a7ebda021afdba620ae7726f5a00
Aleksey Yablokov <ya_al@bk.ru>
2018-01-21 22:33:20 +0300

    refactor AuditoriumControllerTest

--- src/test/java/booking/web/controller/AuditoriumControllerTest.java
+++ src/test/java/booking/web/controller/AuditoriumControllerTest.java
@@ -16,16 +16,22 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Arrays;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static booking.web.controller.AuditoriumController.ENDPOINT;
 import static java.lang.String.format;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -52,13 +58,23 @@ public class AuditoriumControllerTest {
 
     @Test
     public void createAuditorium() throws Exception {
-        String auditoriumName = "Room";
-        int auditoriumId = auditoriumService.getAuditoriums().size() + 1;
-        mvc.perform(post(ENDPOINT)
-                .param("auditoriumName", auditoriumName)
-                .param("seatsNumber", String.valueOf(1000))
-                .param("vipSeats", "1,2,3,4")
-        )
+        String expAuditoriumName = "Room";
+        String expSeatsNumber = String.valueOf(1000);
+        String expVipSeats = "1,2,3,4";
+
+        ResultActions resultActions = mvc.perform(post(ENDPOINT)
+                .param("auditoriumName", expAuditoriumName)
+                .param("seatsNumber", expSeatsNumber)
+                .param("vipSeats", expVipSeats)
+        );
+
+        MvcResult mvcResult = resultActions.andReturn();
+        String content = mvcResult.getResponse().getContentAsString();
+        Matcher m = Pattern.compile("Id: (\\d+)").matcher(content);
+        assertTrue(m.find());
+        long auditoriumId = Long.parseLong(m.group(1));
+
+        resultActions
                 .andExpect(status().isCreated())
                 .andExpect(content().string(format("<h1>Auditorium is created</h1>\n" +
                                 "<p>Id: %d</p>\n" +
@@ -67,25 +83,35 @@ public class AuditoriumControllerTest {
                                 "<p>VIP seats: 1,2,3,4</p>",
                         auditoriumId
                 )));
+
+        Auditorium actAuditorium = auditoriumService.getById(auditoriumId);
+        assertThat(actAuditorium.getName(), equalTo(expAuditoriumName));
+        assertThat(Integer.toString(actAuditorium.getSeatsNumber()), equalTo(expSeatsNumber));
+        assertThat(actAuditorium.getVipSeats(), equalTo(expVipSeats));
+
+        auditoriumService.delete(auditoriumId);
     }
 
     @Test
     public void getAuditoriums() throws Exception {
-        deleteAllAuditoriums();
-        Auditorium auditorium = auditoriumService.create(new Auditorium("Meeting room", 500, Arrays.asList(1, 2, 3)));
         mvc.perform(get("/auditorium"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(format("<h1>Auditoriums</h1>\n" +
-                                "<p>Auditorium</p>\n" +
-                                "<p>Id: %d</p>\n" +
-                                "<p>Name: Meeting room</p>\n" +
-                                "<p>Seats number: 500</p>\n" +
-                                "<p>VIP seats: 1,2,3</p><hr/>\n",
-                        auditorium.getId())));
-    }
-
-    private void deleteAllAuditoriums() {
-        auditoriumService.getAuditoriums().forEach(auditorium -> auditoriumService.delete(auditorium.getId()));
+                .andExpect(content().string("<h1>Auditoriums</h1>\n" +
+                        "<p>Auditorium</p>\n" +
+                        "<p>Id: 1</p>\n" +
+                        "<p>Name: Blue hall</p>\n" +
+                        "<p>Seats number: 500</p>\n" +
+                        "<p>VIP seats: 25,26,27,28,29,30,31,32,33,34,35</p><hr/>\n" +
+                        "<p>Auditorium</p>\n" +
+                        "<p>Id: 2</p>\n" +
+                        "<p>Name: Red hall</p>\n" +
+                        "<p>Seats number: 800</p>\n" +
+                        "<p>VIP seats: 25,26,27,28,29,30,31,32,33,34,35,75,76,77,78,79,80,81,82,83,84,85</p><hr/>\n" +
+                        "<p>Auditorium</p>\n" +
+                        "<p>Id: 3</p>\n" +
+                        "<p>Name: Yellow hall</p>\n" +
+                        "<p>Seats number: 1,000</p>\n" +
+                        "<p>VIP seats: 25,26,27,28,29,30,31,32,33,34,35,75,76,77,78,79,80,81,82,83,84,85,105,106,107,108,109,110,111,112,113,114,115</p><hr/>\n"));
     }
 
     @Test
@@ -99,6 +125,7 @@ public class AuditoriumControllerTest {
                                 "<p>Seats number: 500</p>\n" +
                                 "<p>VIP seats: 1,2,3</p>",
                         auditorium.getId())));
+        auditoriumService.delete(auditorium.getId());
     }
 
     @Test
@@ -116,28 +143,28 @@ public class AuditoriumControllerTest {
 
     @Test
     public void seatsNumberByAuditoriumNameGet() throws Exception {
-        deleteAllAuditoriums();
-        String auditoriumName = "Blue hall";
-        auditoriumService.create(new Auditorium(auditoriumName, 500, Arrays.asList(1, 2, 3)));
+        String auditoriumName = UUID.randomUUID().toString();
+        Auditorium auditorium = auditoriumService.create(new Auditorium(auditoriumName, 500, Arrays.asList(1, 2, 3)));
         mvc.perform(get(format("/auditorium/name/%s/seatsNumber", auditoriumName)))
                 .andExpect(status().isOk())
-                .andExpect(content().string(
+                .andExpect(content().string(format(
                         "<h1>Seats number</h1>\n" +
-                                "<p>Auditorium: Blue hall</p>\n" +
-                                "<p>Seats number: 500</p>"));
+                                "<p>Auditorium: %s</p>\n" +
+                                "<p>Seats number: 500</p>", auditoriumName)));
+        auditoriumService.delete(auditorium.getId());
     }
 
     @Test
     public void seatsNumberByAuditoriumIdGet() throws Exception {
-        deleteAllAuditoriums();
-        String auditoriumName = "Blue hall";
+        String auditoriumName = UUID.randomUUID().toString();
         Auditorium auditorium = auditoriumService.create(new Auditorium(auditoriumName, 500, Arrays.asList(1, 2, 3)));
         mvc.perform(get(format("/auditorium/id/%s/seatsNumber", auditorium.getId())))
                 .andExpect(status().isOk())
-                .andExpect(content().string(
+                .andExpect(content().string(format(
                         "<h1>Seats number</h1>\n" +
-                                "<p>Auditorium: Blue hall</p>\n" +
-                                "<p>Seats number: 500</p>"));
+                                "<p>Auditorium: %s</p>\n" +
+                                "<p>Seats number: 500</p>", auditoriumName)));
+        auditoriumService.delete(auditorium.getId());
     }
 
     @Test
@@ -161,22 +188,23 @@ public class AuditoriumControllerTest {
 
     @Test
     public void deleteExistsAuditorium() throws Exception {
-        String auditoriumName = "Blue hall";
+        String auditoriumName = UUID.randomUUID().toString();
         Auditorium auditorium = auditoriumService.create(new Auditorium(auditoriumName, 500, Arrays.asList(1, 2, 3)));
-        mvc.perform(MockMvcRequestBuilders.delete(format("/auditorium/id/%s/delete", auditorium.getId())))
+        mvc.perform(delete(format("/auditorium/id/%s/delete", auditorium.getId())))
                 .andExpect(status().isOk())
                 .andExpect(content().string(format(
                         "<h1>Auditorium is deleted</h1>\n" +
                                 "<p>Id: %d</p>\n" +
-                                "<p>Name: Blue hall</p>\n" +
+                                "<p>Name: %s</p>\n" +
                                 "<p>Seats number: 500</p>\n" +
                                 "<p>VIP seats: 1,2,3</p>",
-                        auditorium.getId())));
+                        auditorium.getId(), auditoriumName)));
     }
 
     @Test
     public void deleteNotExistsAuditorium() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.delete(format("/auditorium/id/%s/delete", 1234567)))
+        int notExistsAuditoriumId = 1234567;
+        mvc.perform(delete(format("/auditorium/id/%s/delete", notExistsAuditoriumId)))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
                         "<h1>An error occurred</h1>\n" +
