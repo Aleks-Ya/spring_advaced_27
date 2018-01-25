2a376a1ad6e0f5fd122cf09bd9e7c1456a9f0f6d
Aleksey Yablokov <ya_al@bk.ru>
2018-01-25 23:15:49 +0300

    throws appropriate exception in PdfView

--- src/main/java/booking/web/controller/PdfView.java
+++ src/main/java/booking/web/controller/PdfView.java
@@ -4,6 +4,7 @@ package booking.web.controller;
 import booking.domain.Event;
 import booking.domain.Ticket;
 import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.view.AbstractView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -21,9 +23,7 @@ class PdfView extends AbstractView {
         setContentType("application/pdf");
     }
 
-    private void buildPdfDocument(Map<String, Object> model,
-                                  Document document) throws Exception {
-
+    private void buildPdfDocument(Map<String, Object> model, Document document) throws DocumentException {
         @SuppressWarnings("unchecked")
         List<Ticket> tickets = (List<Ticket>) model.get(BookingPdfController.TICKETS_KEY);
 
@@ -38,14 +38,13 @@ class PdfView extends AbstractView {
         } else {
             document.add(new Paragraph(" "));
         }
-
     }
 
 
     @Override
     protected void renderMergedOutputModel(Map<String, Object> model,
                                            HttpServletRequest request,
-                                           HttpServletResponse response) throws Exception {
+                                           HttpServletResponse response) throws DocumentException, IOException {
 
         ByteArrayOutputStream out = createTemporaryOutputStream();
         Document document = new Document(PageSize.A4);
