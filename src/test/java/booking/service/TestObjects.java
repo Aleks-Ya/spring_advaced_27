ad4c2df16f1914ac3bc5631b8ecbebc753e45584
Aleksey Yablokov <ya_al@bk.ru>
2018-02-02 08:16:26 +0300

    add user to TestObjects

--- src/test/java/booking/service/TestObjects.java
+++ src/test/java/booking/service/TestObjects.java
@@ -1,17 +1,24 @@
 package booking.service;
 
 import booking.domain.Auditorium;
+import booking.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Collections;
 
+/**
+ * Provides convenient methods for creating and deleting domain objects in tests.
+ */
 @Component
 public class TestObjects {
 
     @Autowired
     private AuditoriumService auditoriumService;
+    @Autowired
+    private UserService userService;
 
     public Auditorium createBlueHall() {
         return auditoriumService.create(new Auditorium("Blue hall", 15, Arrays.asList(1, 2, 3, 4, 5)));
@@ -28,6 +35,17 @@ public class TestObjects {
     public void deleteAuditorium(Auditorium... auditoriums) {
         for (Auditorium auditorium : auditoriums) {
             deleteAuditorium(auditorium.getId());
+        }
+    }
+
+    public User createJohn() {
+        return userService.register(new User("john@gmail.com", "John Smith",
+                LocalDate.of(1980, 3, 20), "jpass", null));
+    }
+
+    public void deleteUser(User... users) {
+        for (User user : users) {
+            userService.delete(user);
         }
     }
 }
--- src/test/java/booking/service/impl/UserServiceImplTest.java
+++ src/test/java/booking/service/impl/UserServiceImplTest.java
@@ -1,22 +1,18 @@
 package booking.service.impl;
 
+import booking.BaseTest;
 import booking.domain.User;
 import booking.repository.config.DataSourceConfig;
 import booking.repository.config.DbSessionFactoryConfig;
 import booking.repository.config.PropertySourceConfig;
-import booking.repository.config.TestUserServiceConfig;
-import booking.repository.mocks.UserDAOMock;
+import booking.repository.impl.AuditoriumDAOImpl;
+import booking.repository.impl.UserDAOImpl;
+import booking.service.TestObjects;
 import booking.service.UserService;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -31,30 +27,18 @@ import static org.junit.Assert.*;
  * Date: 06/2/16
  * Time: 8:02 PM
  */
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {PropertySourceConfig.class, DataSourceConfig.class, DbSessionFactoryConfig.class, TestUserServiceConfig.class})
-@Transactional
-public class UserServiceImplTest {
+@ContextConfiguration(classes = {PropertySourceConfig.class, DataSourceConfig.class, DbSessionFactoryConfig.class,
+        TestObjects.class, UserServiceImpl.class, AuditoriumServiceImpl.class, AuditoriumDAOImpl.class, UserDAOImpl.class})
+public class UserServiceImplTest extends BaseTest {
 
     @Autowired
     private ConfigurableApplicationContext applicationContext;
 
     @Autowired
-    @Value("#{testUserServiceImpl}")
     private UserService userService;
 
     @Autowired
-    private UserDAOMock userDAOMock;
-
-    @Before
-    public void init() {
-        userDAOMock.init();
-    }
-
-    @After
-    public void cleanup() {
-        userDAOMock.cleanup();
-    }
+    private TestObjects testObjects;
 
     @Test
     public void testRegister() {
@@ -81,15 +65,15 @@ public class UserServiceImplTest {
 
     @Test
     public void testDelete() {
-        User testUser1 = (User) applicationContext.getBean("testUser1");
-        userService.delete(testUser1);
-        User actUser = userService.getUserByEmail(testUser1.getEmail());
+        User user = testObjects.createJohn();
+        userService.delete(user);
+        User actUser = userService.getUserByEmail(user.getEmail());
         assertNull("User should be the same", actUser);
     }
 
     @Test
     public void testUsersGetByName() {
-        User testUser1 = (User) applicationContext.getBean("testUser1");
+        User testUser1 = testObjects.createJohn();
         List<User> before = userService.getUsersByName(testUser1.getName());
         User addedUser = new User(UUID.randomUUID().toString(), testUser1.getName(), LocalDate.now(), "pass", null);
         long registeredId = userService.register(addedUser).getId();
@@ -97,13 +81,15 @@ public class UserServiceImplTest {
         before.add(addedUser.withId(registeredId));
         assertTrue("Users should change", before.containsAll(after));
         assertTrue("Users should change", after.containsAll(before));
+        testObjects.deleteUser(testUser1);
     }
 
     @Test
     public void testGetUserByEmail() {
-        User testUser1 = (User) applicationContext.getBean("testUser1");
-        User foundUser = userService.getUserByEmail(testUser1.getEmail());
-        assertEquals("User should match", testUser1, foundUser);
+        User user = testObjects.createJohn();
+        User foundUser = userService.getUserByEmail(user.getEmail());
+        assertEquals("User should match", user, foundUser);
+        testObjects.deleteUser(user);
     }
 
     @Test
