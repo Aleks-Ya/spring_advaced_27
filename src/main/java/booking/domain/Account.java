bbad892f2bef41850f19c75d693f23fc3ea58a0f
Aleksey Yablokov <ya_al@bk.ru>
2018-02-05 22:59:56 +0300

    replace account.hbm.xml with annotations

--- src/main/java/booking/domain/Account.java
+++ src/main/java/booking/domain/Account.java
@@ -1,14 +1,24 @@
 package booking.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import java.math.BigDecimal;
 import java.util.Objects;
 
 /**
  * Information about money that users possess.
  */
+@Entity
 @SuppressWarnings("unused")
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @OneToOne
     private User user;
     private BigDecimal amount;
 
